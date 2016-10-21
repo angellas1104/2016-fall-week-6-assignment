44b11d1d425cf1531bffd715c1fdb72c14d9ac85
liuanqi <liuanqi@liuanqideMacBook-Air.local>
2016-10-21 12:46:23 -0400

    assignment6

--- 6.1/script.js
+++ 6.1/script.js
@@ -15,4 +15,60 @@ var plot = d3.select('.canvas')
     .attr('transform','translate(' + m.l + ',' + m.t + ')');
 
 //Import data and parse
-d3.csv('../data/olympic_medal_count.csv',parse,dataLoaded);
+d3.csv('../data/olympic_medal_count.csv',parse,data);
+
+function parse(d){
+	return{
+		country:d['Country'],
+		country1900:+d['1900'],
+		country1960:+d['1960'],
+		country2012:+d['2012'],
+	};
+}
+function data(err,rows){
+	console.table(rows);
+	
+	var sortarr=rows.sort(function(a,b){
+		return b.country2012-a.country2012;
+	}),
+		newarr=sortarr.slice(0,5);
+	console.log(sortarr,newarr)
+
+	var minY=d3.extent(newarr,function(d){return d.country2012}),
+		maxY=d3.max(newarr,function(d){return d.country2012}),
+		x=newarr.map(function(d){return d.country});
+	var scaleY=d3.scaleLinear()
+		.domain([0,maxY+20])
+		.range([h,0]),
+		scaleX=d3.scaleBand()
+		.range([0,w])
+		.domain(x)
+		.paddingOuter(.3)
+		.paddingInner(.6);
+
+
+	var rectangle=plot.selectAll('rect')
+		.data(newarr)
+		.enter().append('rect')
+		.attr('class','bar')
+		.style('width',scaleX.bandwidth())
+		.style('height',function(d){return h-scaleY(d.country2012)})
+		.attr('y',function(d){return scaleY(d.country2012)})
+		.attr('x',function(d){return scaleX(d.country)});
+
+	//add axis
+	var axisX=d3.axisBottom()
+		.scale(scaleX),
+		axisY=d3.axisLeft()
+		.scale(scaleY)
+		.tickSize(-w);
+		
+	var axisNodeX=plot.append('g')
+		.attr('class','axis')
+		.attr('transform','translate(' + 0 + ',' + h+ ')'),
+		axisNodeY=plot.append('g')
+		.attr('class','axis')
+		.attr('transform','translate(' + 0 + ',' + 0+ ')');
+		axisX(axisNodeX);
+		axisY(axisNodeY);
+}
